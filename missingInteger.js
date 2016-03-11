6ee1c95aacc7e926c20cb39e145b393ca432c41d
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-03-11 13:13:03 -0500

    solutions

---
+++ missingInteger.js
@@ -0,0 +1,44 @@
+// Write a function:
+//
+// function solution(A);
+//
+// that, given a non-empty zero-indexed array A of N integers, returns the minimal positive integer (greater than 0) that does not occur in A.
+//
+// For example, given:
+//
+//   A[0] = 1
+//   A[1] = 3
+//   A[2] = 6
+//   A[3] = 4
+//   A[4] = 1
+//   A[5] = 2
+// the function should return 5.
+//
+// Assume that:
+//
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+// Complexity:
+//
+// expected worst-case time complexity is O(N);
+// expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+// Elements of input arrays can be modified.
+
+
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 4.0.0)
+    var min = 1;
+        A.sort(function(a,b){
+           // Sort the array explicit way
+           return a - b;
+        });
+
+        for (var i in A) {
+            if (A[i] > -1 && A[i] == min) {
+                    min++;
+            }
+        }
+
+        return min;
+}
