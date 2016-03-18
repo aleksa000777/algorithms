a563b829c0eb09c916572c7965812253c4c9eaa4
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-03-18 11:08:47 -0400

    binary search

--- binary_search.js
+++ binary_search.js
@@ -17,3 +17,21 @@ function searchInsert(arr,target){
 }
 
 searchInsert([1,2,4,5,6],9)
+
+
+function binary_search(items, value){
+  var startIndex = 0;
+  var stopIndex = items.length-1;
+  var middle = Math.floor((stopIndex + startIndex)/2);
+
+  while(items[middle] != value && startIndex < stopIndex){
+    if(value < items[middle]){
+      stopIndex = middle - 1;
+    }
+    else if(value > items[middle]){
+      startIndex = middle + 1;
+    }
+    middle = Math.floor((stopIndex + startIndex)/2);
+  }
+  return (items[middle] != value) ? -1 : middle;  
+}
